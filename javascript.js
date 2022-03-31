b05cb7b04548aa62b0f8e800cef46ffc741f156e
Arturo Rivera <arturo.riverar97@gmail.com>
2022-03-31 11:53:28 -0600

    Create a function getRandomInteger
    In light of the fact that computerPlay() was carrying out two actions,
    it was decided to create a helper function to compute a random integer
    so that computerPlay would only have to assign Rock, Paper or Scissors
    according to the value yielded by getRandomInteger.
    
    The getRandomInteger function leverages the functions Math.floor and
    Math.random. The former rounds a number DOWNWARDS to the nearest
    integer, whereas the latter computes a number between [0,1), so it had
    to be multiplied by 3 to achieve the desired range.
    
    On the other hand, the range was changed to [0,2]; therefore, the
    matches would be as follows:
    
    0 - Rock
    1 - Paper
    2 - Scissors

--- javascript.js
+++ javascript.js
@@ -1,11 +1,25 @@
-function computerPlay() {
+function getRandomInteger() {
     // Extremos del intervalo de numeros aleatorios
-    let numMax = 3.1;
-    let numMin = 1;
+    let numMax = 3;
 
     // Retorna un entero aleatorio entre min (incluido) y max (excluido)
     // ¡Usando Math.round() te dará una distribución no-uniforme!
-    return Math.floor(Math.random()*(numMax-numMin)+numMin)
+    return Math.floor(Math.random()*numMax);
+}
+
+function computerPlay() {
+    // Se llama getRandomInteger() y se almacena su valor
+    let randomInteger = getRandomInteger();
+
+    // Se obtiene piedra papel o tijera segun el numero conseguido
+    if (randomInteger == 0) {
+        return 'Rock';
+    } else if(randomInteger == 1) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+
 }
 
 console.log(computerPlay())
